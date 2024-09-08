90d1bf8b4011bc4294661dda4e6e125577baa19c
GhulamMD <ghulam.mohammad576@gmail.com>
2024-09-08 14:40:42 +0530

    added a calculator button

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,15 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
 
   return (
     <>
-        <h1>This is my Website </h1>
+        <h1>Calculator</h1>
+    
+        <button type="button" class="btn btn-success">Calculate Now</button>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit in, quaerat eum veniam aperiam assumenda non deserunt eos perferendis earum magni harum illo fugit, sequi repellat temporibus quas vel rem? Explicabo totam quibusdam rem, aliquid a recusandae illepellat nostrum blanditiis sed ex dolore! Rem, consectetur iusto? Enim et praesentium odio incidunt culpa qui tempora temporibus, voluptatum aperiam labore, beatae quasi?</p>
 
 
-
-        <h2>My data is uploaded</h2>
-
-        <div>
-        <h1>Indian Names Table</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Aarav</td>
-                <td>28</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Vivaan</td>
-                <td>32</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Vihaan</td>
-                <td>24</td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Aadhya</td>
-                <td>29</td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Isha</td>
-                <td>31</td>
-            </tr>
-        </tbody>
-    </table></div>
-        
     </>
   )
 }
